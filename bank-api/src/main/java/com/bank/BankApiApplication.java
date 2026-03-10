6c095ad7f33af9fc9d7f8cc9850ab59965b0678e
Miguel <miguel.lopez81@correo.tdea.edu.co>
2026-03-09 22:34:56 -0500

    fix: configurar aplicación para funcionar sin MongoDB
    - Excluir autoconfiguración de MongoDB en @SpringBootApplication
    - Usar almacenamiento en memoria para bitácora (InMemoryBitacoraRepositoryAdapter)
    - Agregar exclusiones: MongoAutoConfiguration, MongoDataAutoConfiguration, MongoRepositoriesAutoConfiguration
    - Configuración para desarrollo sin dependencia de MongoDB externo
    
    La aplicación ahora inicia correctamente usando:
    - H2 Database (SQL en memoria) para datos relacionales
    - Almacenamiento en memoria para bitácora
    - Todos los endpoints funcionales
    - Swagger UI disponible en http://localhost:8080/swagger-ui.html

--- bank-api/src/main/java/com/bank/BankApiApplication.java
+++ bank-api/src/main/java/com/bank/BankApiApplication.java
@@ -2,9 +2,16 @@ package com.bank;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.data.mongo.MongoDataAutoConfiguration;
+import org.springframework.boot.autoconfigure.data.mongo.MongoRepositoriesAutoConfiguration;
+import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {
+    MongoAutoConfiguration.class,
+    MongoDataAutoConfiguration.class,
+    MongoRepositoriesAutoConfiguration.class
+})
 @EnableScheduling
 public class BankApiApplication {
 
